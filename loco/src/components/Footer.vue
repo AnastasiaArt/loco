cb6b7462a128d7374c3e327da68ed93ab5b4cfc6
Anastasia Art <art-nastya@inbox.ru>
2019-09-30 02:53:42 +0300

    добавила картинки

--- loco/src/components/Footer.vue
+++ loco/src/components/Footer.vue
@@ -2,5 +2,10 @@
     <footer class="footer">
         <p><span>Спецпроект</span> #UAxLoco<br>
         Раccкажи о нем друзьям:</p>
+        <ul class="footer__social">
+            <li> <a href="#"><img alt="Facebook" src="../../public/static/facebook.svg" width="70" height="70"></a></li>
+            <li><a href="#"><img alt="Facebook" src="../../public/static/instagram.svg" width="70" height="70"></a></li>
+            <li><a href="#"><img alt="Facebook" src="../../public/static/vk.svg" width="70" height="70"></a></li>
+        </ul>
     </footer>
 </template>
